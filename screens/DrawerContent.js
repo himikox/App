2ee444d4e67541ab62646d2e74bbaff9bc486060
Himikox <ikram.mkaissi@insat.u-carthage.tn>
2021-03-27 12:53:17 +0100

    first commit

---
+++ screens/DrawerContent.js
@@ -0,0 +1,172 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import {
+    useTheme,
+    Avatar,
+    Title,
+    Caption,
+    Paragraph,
+    Drawer,
+    Text,
+    TouchableRipple,
+    Switch
+} from 'react-native-paper';
+import {
+    DrawerContentScrollView,
+    DrawerItem
+} from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/Ionicons';
+//import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import{ AuthContext } from '../components/context';
+
+export function DrawerContent(props) {
+
+    const paperTheme = useTheme();
+
+    const { signOut, toggleTheme } = React.useContext(AuthContext);
+
+    return(
+        <View style={{flex:1}}>
+            <DrawerContentScrollView {...props}>
+                <View style={styles.drawerContent}>
+                    <View style={styles.userInfoSection}>
+                        <View style={{flexDirection:'row',marginTop: 15}}>
+                            <Avatar.Image
+                                source={{
+                                    uri: 'https://api.adorable.io/avatars/50/abott@adorable.png'
+                                }}
+                                size={50}
+                            />
+                            <View style={{marginLeft:15, flexDirection:'column'}}>
+                                <Title style={styles.title}>John Doe</Title>
+                                <Caption style={styles.caption}>@j_doe</Caption>
+                            </View>
+                        </View>
+
+
+                    </View>
+
+                    <Drawer.Section style={styles.drawerSection}>
+                        <DrawerItem
+                            icon={({color, size}) => (
+                                <Icon
+                                name="ios-home"
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Home"
+                            onPress={() => {props.navigation.navigate('Home')}}
+                        />
+                        <DrawerItem
+                            icon={({color, size}) => (
+                                <Icon
+                                    name="ios-aperture"
+                                    color={color}
+                                    size={size}
+                                />
+                            )}
+                            label="DOCTORSINA"
+                            onPress={() => {props.navigation.navigate('Details')}}
+                        />
+                        <DrawerItem
+                            icon={({color, size}) => (
+                                <Icon
+                                name="ios-person"
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Profile"
+                            onPress={() => {props.navigation.navigate('Profile')}}
+                        />
+                        <DrawerItem
+                            icon={({color, size}) => (
+                                <Icon
+                                name="ios-bookmark"
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Bookmarks"
+                            onPress={() => {props.navigation.navigate('BookmarkScreen')}}
+                        />
+                        <DrawerItem
+                            icon={({color, size}) => (
+                                <Icon
+                                name="ios-settings"
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Settings"
+                            onPress={() => {props.navigation.navigate('SettingsScreen')}}
+                        />
+
+                    </Drawer.Section>
+
+                </View>
+            </DrawerContentScrollView>
+            <Drawer.Section style={styles.bottomDrawerSection}>
+                <DrawerItem
+                    icon={({color, size}) => (
+                        <Icon
+                        name="ios-add"
+                        color={color}
+                        size={size}
+                        />
+                    )}
+                    label="Sign Out"
+                    onPress={() => {signOut()}}
+                />
+            </Drawer.Section>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    drawerContent: {
+      flex: 1,
+    },
+    userInfoSection: {
+      paddingLeft: 20,
+    },
+    title: {
+      fontSize: 16,
+      marginTop: 3,
+      fontWeight: 'bold',
+    },
+    caption: {
+      fontSize: 14,
+      lineHeight: 14,
+    },
+    row: {
+      marginTop: 20,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    section: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginRight: 15,
+    },
+    paragraph: {
+      fontWeight: 'bold',
+      marginRight: 3,
+    },
+    drawerSection: {
+      marginTop: 50,
+    },
+    bottomDrawerSection: {
+        marginBottom: 15,
+        borderTopColor: '#529ecf',
+        borderTopWidth: 1
+    },
+    preference: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingVertical: 12,
+      paddingHorizontal: 16,
+    },
+  });
